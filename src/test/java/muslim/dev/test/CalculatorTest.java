a472031b25f75bbde94efd9bb5434d9387971b8f
herman-xphp <hermanxphp@gmail.com>
2025-02-02 14:22:29 +0800

    Menggunakan Assertions

--- src/test/java/muslim/dev/test/CalculatorTest.java
+++ src/test/java/muslim/dev/test/CalculatorTest.java
@@ -1,5 +1,8 @@
 package muslim.dev.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
@@ -8,6 +11,24 @@ public class CalculatorTest {
 
   @Test
   void testAddSuccess() {
-    calculator.add(10, 10);
+    Integer result = calculator.add(10, 10);
+    assertEquals(20, result);
+
+    // if (result != 20) {
+    // throw new RuntimeException("Test failed");
+    // }
   }
+
+  @Test
+  void testDivideSuccess() {
+    Integer result = calculator.divide(100, 10);
+    assertEquals(10, result);
+  }
+
+  @Test
+  void testDivideFailed() {
+    assertThrows(
+        IllegalArgumentException.class, () -> calculator.divide(100, 0));
+  }
+
 }
