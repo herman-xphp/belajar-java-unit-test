a1449f411cf38e057ce396f45604f0f1e2056ed1
herman-xphp <hermanxphp@gmail.com>
2025-03-04 18:11:19 +0800

    Parallel execution test

--- src/test/java/muslim/dev/test/SlowTest.java
+++ src/test/java/muslim/dev/test/SlowTest.java
@@ -4,12 +4,27 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
+@Execution(value = ExecutionMode.CONCURRENT)
 public class SlowTest {
 
   @Test
   @Timeout(value = 5, unit = TimeUnit.SECONDS)
-  void testSlow() throws InterruptedException {
-    // Thread.sleep(10_000);
+  void testSlow1() throws InterruptedException {
+    Thread.sleep(3_000);
+  }
+
+  @Test
+  @Timeout(value = 5, unit = TimeUnit.SECONDS)
+  void testSlow2() throws InterruptedException {
+    Thread.sleep(4_000);
+  }
+
+  @Test
+  @Timeout(value = 5, unit = TimeUnit.SECONDS)
+  void testSlow3() throws InterruptedException {
+    Thread.sleep(2_000);
   }
 }
