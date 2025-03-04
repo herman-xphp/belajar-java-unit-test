5678d90ad52b84ee4dd68f0ed032fec8c1277c2f
herman-xphp <hermanxphp@gmail.com>
2025-03-04 16:38:48 +0800

    Lifecycle Test

--- src/test/java/muslim/dev/test/OrderedTest.java
+++ src/test/java/muslim/dev/test/OrderedTest.java
@@ -1,28 +1,47 @@
 package muslim.dev.test;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+
+  private int counter = 0;
+
+  @BeforeAll
+  void beforeAll() {
+    System.out.println("Before all test");
+  }
+
+  @AfterAll
+  void afterAll() {
+    System.out.println("After all test");
+  }
 
   @Test
   @Order(1)
   void test3() {
-
+    counter++;
+    System.out.println(counter);
   }
 
   @Test
   @Order(2)
   void test2() {
-
+    counter++;
+    System.out.println(counter);
   }
 
   @Test
   @Order(3)
   void test1() {
-
+    counter++;
+    System.out.println(counter);
   }
 }
